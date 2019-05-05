c813b5e5bdafccfd2e330d110730d801eba3d738
steve <meitiever@gmail.com>
2019-05-05 10:50:33 +0800

    [New Feature] extend new example.

--- EigenExample/EigenExample.cpp
+++ EigenExample/EigenExample.cpp
@@ -13,6 +13,40 @@
 using namespace Eigen;
 using namespace std;
 
+void basic_operations()
+{
+	Eigen::MatrixXd mat(2, 2);
+	mat << 0, 1, 2, 3;
+	mat.resize(3, 3);
+	cout << mat << endl;
+	mat << 0, 1, 2, 3;
+	mat.conservativeResize(3, 3);
+	cout << mat << endl;
+}
+
+void advanced_initialization()
+{
+	Eigen::MatrixXd mat(2, 2);
+	mat(0, 0) = 3;
+	mat(0, 1) = 2.5;
+	mat(1, 0) = -1;
+	mat(1, 1) = mat(0,0) + mat(0,1);
+
+	Eigen::MatrixXd mat1(2, 2);
+	mat1 << 1, 2, 3, 4;
+
+	Eigen::VectorXd vec(5);
+	vec << 1, 2, 3, 4, 5;
+
+	Eigen::MatrixXd mat2 = Eigen::MatrixXd::Zero(1, 1);
+	Eigen::MatrixXd mat3 = Eigen::MatrixXd::Random(3, 3);
+	Eigen::MatrixXd mat4 = Eigen::MatrixXd::Zero(3, 3);
+	mat4 = (mat4 + Eigen::MatrixXd::Constant(3, 3, 1.2)) * 50;
+	Eigen::MatrixXd mat5 = Eigen::MatrixXd::Random(2, 3);
+	mat5 = (MatrixXd(2, 2) << 0, 1, 2, 3).finished() * mat5;
+	cout << mat5 << endl;
+}
+
 void ex3()
 {
 	Eigen::Quaternionf q(2., 0., 1., -3.);
@@ -48,8 +82,10 @@ void ex1()
 						1.0 , -1.0 , 1.0 ,
 						1.0 , 1.0 , 1.0 ,
 						-1.0 , 1.0 , 1.0 };
-	MatrixXf mVertices = Map < Matrix <float, 3, 8 > >(arrVertices);
-	Eigen::Transform <float, 3, Affine> t = Eigen::Transform <float, 3, Affine>::Identity();	t.scale(0.8f);
+	MatrixXf mVertices = Map < Matrix <float, 3, 8 > >(arrVertices);
+
+	Eigen::Transform <float, 3, Affine> t = Eigen::Transform <float, 3, Affine>::Identity();
+	t.scale(0.8f);
 	t.rotate(Eigen::AngleAxisf(0.25f * M_PI, Eigen::Vector3f::UnitX()));
 	t.translate(Eigen::Vector3f(1.5, 10.2, -5.1));
 
@@ -64,4 +100,5 @@ int main()
 	ex1();
 	ex2();
 	ex3();
+	advanced_initialization();
 }
